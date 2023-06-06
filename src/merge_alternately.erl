53f27cc6984099aa437180752ee972098b87d1f8
wangzihao223 <1600964393@qq.com>
2023-06-06 14:56:58 +0800

    交替合并字符串

---
+++ src/merge_alternately.erl
@@ -0,0 +1,18 @@
+-module(merge_alternately).
+-export([main/2]).
+
+main(W1, W2) ->
+    W3 = binary:bin_to_list(W1),
+    W4 = binary:bin_to_list(W2),
+    W5 = loop(W3, W4, []),
+    unicode:characters_to_binary(W5).
+
+
+loop([], [W2|Next2], R) ->
+    lists:reverse(R, [W2|Next2]);
+loop([W1|Next1], [], R) ->
+    lists:reverse(R, [W1|Next1]);
+loop([], [], R) ->
+    lists:reverse(R);
+loop([W1|Next1], [W2|Next2], R) ->
+loop(Next1, Next2, [W2, W1 | R]).
