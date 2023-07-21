4e4069cf721450013e41a68208a9f049c13cd556
wangzihao <1600964393@qq.com>
2023-07-22 01:38:49 +0800

    860. 柠檬水找零

---
+++ src/lemonade_change.erl
@@ -0,0 +1,50 @@
+
+-module(lemonade_change).
+
+% 在柠檬水摊上，每一杯柠檬水的售价为 5 美元。顾客排队购买你的产品，（按账单 bills 支付的顺序）一次购买一杯。
+
+% 每位顾客只买一杯柠檬水，然后向你付 5 美元、10 美元或 20 美元。你必须给每个顾客正确找零，也就是说净交易是每位顾客向你支付 5 美元。
+
+% 注意，一开始你手头没有任何零钱。
+
+% 给你一个整数数组 bills ，其中 bills[i] 是第 i 位顾客付的账。如果你能给每位顾客正确找零，返回 true ，否则返回 false 。
+
+%  [5,5,5,10,20]
+% 
+-export([lemonade_change/1]).
+
+
+-spec lemonade_change(Bills :: [integer()]) -> boolean().
+lemonade_change(Bills) ->
+  sim_process(Bills, #{5=>0, 10=>0}).
+
+
+
+sim_process([B | NextBill], MoneyBox) ->
+  case B of
+    5 ->
+      V =  map_get(B, MoneyBox),
+      NewMoneyBox = MoneyBox#{5:= V + 1},
+      sim_process(NextBill, NewMoneyBox);
+    10 ->
+      V1 = map_get(5, MoneyBox),
+      V2 = map_get(10, MoneyBox),
+      if V1 == 0 ->
+          false;
+        true ->
+          NewMoneyBox = MoneyBox#{5:= V1 - 1, 10:= V2 + 1},
+          sim_process(NextBill, NewMoneyBox)
+      end;
+    20 ->
+      V1 = map_get(5, MoneyBox),
+      V2 = map_get(10, MoneyBox),
+      if V2 =/= 0 , V1 =/= 0 ->
+          sim_process(NextBill, MoneyBox#{5:= V1 - 1, 10:= V2 - 1});
+        V1 > 2 ->
+          sim_process(NextBill, MoneyBox#{5:= V1 - 3, 10:= V2});
+        true ->
+          false
+      end
+  end;
+sim_process([], _MoneyBox) -> true.
+
