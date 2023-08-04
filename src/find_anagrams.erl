13b00c8e1602efa2d555deb2477ee76929562e9a
wangzihao <1600964393@qq.com>
2023-08-04 14:18:28 +0800

    .找到字符串中所有字母异位词

--- src/find_anagrams.erl
+++ src/find_anagrams.erl
@@ -17,48 +17,67 @@
 find_anagrams(S, P) ->
   SL = binary_to_list(S),
   PL = binary_to_list(P),
-  SourceSet = sets:from_list(PL, [{version, 2}]),
-  move(queue:new(), SL, SourceSet, sets:new([{version, 2}]), [], 0).
+  Map = make_map(PL, #{}),
+  start(SL, length(PL), Map, []).
 
 
-move(Win, [Letter | NextLetter], SourceSet, Set, Res, I) ->
+make_map([L | NextL], Map) ->
+  make_map(NextL, Map#{L => maps:get(L, Map, 0) + 1});
+make_map([], Map) -> Map.
 
-  case sets:is_element(Letter, SourceSet) of
-    true ->
-      case sets:is_element(Letter, Set) of
-        true ->
-          {Win1, Set1} = resize_window(Win, Letter, Set, I),
-          if SourceSet == Set ->
-              {_, Index} =  queue:head(Win),
-              io:format("Index is ~p I is ~p ~n", [Index, I]),
-              move(Win1, NextLetter, SourceSet, Set1, [Index | Res], I + 1);
-            true ->
-              move(Win1, NextLetter, SourceSet, Set1, Res, I + 1)
-          end;
-        false ->
-          move(queue:in({Letter, I}, Win), NextLetter, SourceSet, sets:add_element(Letter, Set), Res, I + 1)
-      end;
-    false ->
-      if SourceSet == Set ->
-          {_, Index} =  queue:head(Win),
-          io:format("Index is ~p ~n", [Index]),
-          move(queue:new(), NextLetter, SourceSet, sets:new([{version, 2}]), [Index | Res], I + 1);
-        true->
-          move(queue:new(), NextLetter, SourceSet, sets:new([{version, 2}]), Res, I + 1)
-      end
-  end;
-move(Win, [], SourceRes, SourceRes, Res, _) ->
-  {_, Index} =  queue:head(Win),
-  [Index | Res];
-move(_, [], _, _, Res, _) -> Res.
+start(SL, Length, Map, Res) ->
+  init_and_move(SL, Length, Map, 0, Res).
 
-resize_window(Win, Letter, Set, Index) ->
-  case sets:is_element(Letter, Set) of
-    true ->
-        {{value, {V, I}}, Win1} = queue:out(Win),
-        io:format("out ~p ~n", [I]),
-        Set1 = sets:del_element(V, Set),
-        resize_window(Win1, Letter, Set1, Index);
-    false ->
-      {queue:in({Letter, Index}, Win), sets:add_element(Letter, Set)}
+init_and_move(L, Length, Map, I, Res) ->
+  case init_window(queue:new(), #{}, L, Length, Map, I, Length) of
+    false -> Res;
+    {Win1, WinMap1, Remain, I1} ->
+      Res1 = update_res(Res, Map, WinMap1, Win1),
+      move_window(Win1, WinMap1, Map, Remain, I1, Res1, Length)
   end.
+
+init_window(_Window, _WinMap, [], Length, _Map, _Index, _SourceLength) when Length > 0 -> false;
+init_window(Window, WinMap, [L | NextL], Length, Map, Index, SourceLength) when Length > 0->
+  case maps:is_key(L, Map) of
+    true ->
+      init_window(queue:in([L, Index], Window), WinMap#{L => maps:get(L, WinMap, 0) + 1}, NextL, Length - 1, Map, Index + 1,  SourceLength);
+    false ->
+      init_window(queue:new(), #{}, NextL, SourceLength, Map, Index + 1, SourceLength)
+  end;
+init_window(Window, WinMap, Remain, _, _, Index, _SourceLength) -> {Window, WinMap, Remain, Index}.
+
+move_window(Win, WinMap, Map, [L | NextL], I, Res, Length) ->
+  case maps:is_key(L, Map) of
+    false ->
+      init_and_move(NextL, Length, Map, I + 1, Res);
+    true ->
+      {{value, [K, _]}, Win1} = queue:out(Win),
+      Win2 = queue:in([L, I], Win1),
+      WinMap1 = WinMap#{K := map_get(K, WinMap) - 1},
+      WinMap2 = WinMap1#{L => maps:get(L, WinMap1, 0) + 1},
+      Res1 = update_res(Res, Map, WinMap2, Win2),
+      move_window(Win2, WinMap2, Map, NextL, I + 1, Res1, Length)
+  end;
+move_window(_, _, _, [], _, Res, _) -> Res.
+
+equal(Map, WinMap) ->
+  case maps:next(Map) of
+    none -> true;
+    {K, V, Next} ->
+      case maps:is_key(K, WinMap) of
+        true ->
+          case V == map_get(K, WinMap) of
+            true -> equal(Next, WinMap);
+            false -> false
+          end;
+        false -> false
+      end
+  end.
+
+update_res(Res, Map, WinMap, Win) ->
+    case equal(maps:iterator(Map), WinMap) of
+      true ->
+        [_, I2] = queue:head(Win),
+        [I2 | Res];
+      false -> Res
+    end.
