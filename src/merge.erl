2a8c680662b318e3a2f4ed851b261e35990ed7b2
wangzihao <1600964393@qq.com>
2023-08-27 18:40:40 +0800

    合并区间

---
+++ src/merge.erl
@@ -0,0 +1,13 @@
+-module(merge).
+
+-export([merge/1]).
+% 合并区间
+merge(Intervals) ->
+    [Head | Next] = lists:sort(Intervals),
+    merge1(Next, [Head]).
+
+merge1([[L1, R1] | Next], [[L2, R2] | Next1]) when R2 >= L1 ->
+    merge1(Next, [[L2, max(R2, R1)] | Next1]);
+merge1([[L1, R1] | Next], [[L2, R2] | Next1]) ->
+     merge1(Next, [[L1, R1], [L2, R2] | Next1]);
+merge1([], Res) -> lists:reverse(Res).
