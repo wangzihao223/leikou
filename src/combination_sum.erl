a806f4374ae8374d0d1bd3ea69bb60175fa7c40c
wangzihao <1600964393@qq.com>
2023-09-01 01:27:30 +0800

    组合总和

--- src/combination_sum.erl
+++ src/combination_sum.erl
@@ -2,22 +2,14 @@
 % 组合总和
 -export([combination_sum/2]).
 
-combination_sum(Candiates, Target) -> 
-    Candiates1 = lists:reverse(lists:sort(Candiates)),
-    Candiates2 = [H || H <-Candiates1, H =< Target],
-    % Set = select(Candiates2, Target, Target, [], sets:new([{version, 2}])),
-    {_,_, Res} = select(Candiates2, Target, Target, [], sets:new([{version, 2}])),
-    sets:to_list(Res).
+combination_sum(Candiates, Target) ->
+    Candiates1 = lists:sort(Candiates),
+    dfs(Target, Candiates1, Candiates1, [], []).
 
-select([N | Next], Sum, Target, R, Res) when Sum > N ->
-    io:format("R2 is ~p  N is ~p Sum is ~p ~n", [R, [N|R], Sum -N]),
-    {Sum1, _R1, Res1}= select([N | Next], Sum - N , Target, [N | R], Res),
-    select(Next, Sum -Sum1 , Target,  R, Res1);
-select([N | Next], Sum, Target, R, Res) when Sum == N ->
-    io:format("R1 is ~p  N is ~p Sum ~p ~n", [R, [N|R], Sum]),
-    select(Next, Target, Target,  R, sets:add_element([N | R], Res));
-    % select(Next, Sum, Target, R, Res1);
-select([_ | Next], Sum, Target,  N, Res) ->
-    % io:format("R is ~p  N is ~p Sum ~p ~n", [R, N, Sum]),
-    select(Next, Sum,  Target, N, Res);
-select([], Sum, _Target, R, Res) -> {Sum, R, Res}.
+
+dfs(Root, [C | Next], Candiates, Path, R) when Root - C > 0 ->
+    R1 = dfs(Root - C, Candiates, Candiates, [C | Path], R),
+    dfs(Root, Next, Next, Path, R1);
+dfs(Root, [C | _], _, Path, R) when Root - C == 0->
+    [[C | Path] | R];
+dfs(_, _, _, _, R) ->R.
