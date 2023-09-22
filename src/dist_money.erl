2457d3ac0b2a243fe46dd2024b5593bc8980b4cd
wangzihao <1600964393@qq.com>
2023-09-22 10:00:31 +0800

    将钱分给最多的儿童

---
+++ src/dist_money.erl
@@ -0,0 +1,11 @@
+-module(dist_money).
+-export([dist_money/2]).
+
+% 将钱分给最多的儿童
+-spec dist_money(Money :: integer(), Children :: integer()) -> integer().
+dist_money(Money, Children) ->
+    if Children * 8  < Money -> Children - 1;
+        Money < Children -> -1;
+        Money == 8 * Children -4 -> Children - 2;
+        true -> (Money - Children) div 7
+    end.
